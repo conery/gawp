71978665c06dcd2db4f287121d595dc35e27fdac
John Conery <jconery@icloud.com>
2026-08-27 20:46:58 -0700

    Add function that gets measurement points, add unit test for that function

--- src/gawp/io.py
+++ src/gawp/io.py
@@ -10,33 +10,68 @@ from shapely.geometry import Point
 
 # TODO  define column names in config file
 
-def read_cell_positions(f:pd.ExcelFile, sheet:str="Position", category:str="Surface"):
+def parse_positions(f:pd.ExcelFile, sheet_name:str="Position"):
     '''
-    Read cell location data from an XLS file exported by Imaris.  Returns a Pandas
-    data frame with the locations of all the rows in the specified category.
-     
-    Raises an exception if the file does not have a sheet named "Position" or if that
-    sheet does not have the columns we need.
+    Read location data from an XLS file exported by Imaris.  Returns a Pandas
+    data frame with all the rows from the sheet that has positions.  By default
+    the name of that sheet is "Position" but another name can be passed.
 
+    TODO:  get the sheet name and column names from a config file
+     
     Arguments:
-        f:  the file to read from
-        sheet (optional): the name of the sheet that has the data (default: "Position")
+        f:  the XLS file to read from
+        sheet_name (optional): the name of the sheet that has the data (default: "Position")
+
+    Returns:
+        a Pandas frame containing the ID, X and Y location, and category from each row
+    '''
+    assert sheet_name in f.sheet_names, f"spreadheet is missing the '{sheet_name}' sheet"
+    sf = f.parse(sheet_name, header=1)
+
+    required_cols = ['ID', 'Name', 'Category', 'Surpass Object', 'Position X', 'Position Y']
+    assert set(required_cols) < set(sf.columns), f"sheet is missing one or more required colums ({required_cols})"
+
+    return sf[required_cols]
+
+def get_cell_positions(df: pd.DataFrame, category:str="Surface"):
+    '''
+    Extract the cell location data from a DataFrame created from the Position sheet in 
+    an XLS file exported by Imaris.  
+     
+    Arguments:
+        df:  the data frame with position data
         category (optional): the type of data to read (default: "Surface")
 
     Returns:
-        a Pandas frame containing unique IDs for each cell and a Point object with the
+        a GeoDataFrame containing unique IDs for each cell and a Point object with the
         x and y coordinates of the cell.
     '''
-    assert "Position" in f.sheet_names, "spreadheet is missing the 'Position' sheet"
-    sheet = f.parse("Position", header=1)
-
-    for col in ['Category', 'Surpass Object', 'ID', 'Position X', 'Position Y']:
-        assert col in sheet, f"sheet does not have a {col} column"
-
-    pf = sheet[sheet['Category']==category]
+    pf = df[df['Category']==category]
     pf.index = range(len(pf))
 
     return gp.GeoDataFrame({
         'id': pf['Surpass Object'] + pf['ID'].apply(str),
         'point': [Point(pf.loc[i]['Position X'], pf.loc[i]['Position Y']) for i in range(len(pf))]
     }).set_geometry('point')
+
+def get_measurements(df: pd.DataFrame, category:str="MeasurementPoint"):
+    '''
+    Extract the measurement locations from a DataFrame created from the Position sheet in 
+    an XLS file exported by Imaris.
+     
+    Arguments:
+        df:  the data frame with position data
+        category (optional): the type of data to read (default: "MeasurementPoint")
+
+    Returns:
+        a GeoDataFrame containing unique IDs for each measurement ppint and a Point object with the
+        x and y coordinates of the measurement.
+    '''
+    pf = df[df['Category']==category]
+    pf.index = range(len(pf))
+
+    return gp.GeoDataFrame({
+        'name': pf['Name'],
+        'point': [Point(pf.loc[i]['Position X'], pf.loc[i]['Position Y']) for i in range(len(pf))]
+    }).set_geometry('point')
+
--- tests/unit/test_io.py
+++ tests/unit/test_io.py
@@ -6,7 +6,7 @@ import pytest
 
 import pandas as pd
 import geopandas as gp
-from gawp.io import read_cell_positions
+from gawp.io import parse_positions, get_cell_positions, get_measurements
 
 TEST_DATA = 'data//PRG-1_herm_g05_sample.xlsx'
 
@@ -23,23 +23,38 @@ class TestIO:
         assert len(xls_file.sheet_names) == 39
         assert 'Position' in xls_file.sheet_names
 
-    def test_parse_position_sheet(self, xls_file):
+    def test_parse_positions(self, xls_file):
         """
-        Parse the Position sheet in the XLS file
+        Test the function that parses the Position sheet in the XLS file
         """
-        sheet = xls_file.parse('Position', header=1)
-        assert len(sheet) == 4596
-        assert {'Position X', 'Position Y', 'Category'} < set(sheet.columns)
+        sf = parse_positions(xls_file)
+        assert len(sf) == 4596
+        assert {'Position X', 'Position Y', 'Category'} < set(sf.columns)
 
-    def test_read_cell_positions(self, xls_file):
+    def test_cell_positions(self, xls_file):
         """
-        Test the read_cell_positions function, which gets x and y coordinates
+        Test the get_cell_positions function, which gets x and y coordinates
         of cells from the Position sheet and returns them in a GeoPandas frame
         """
-        df = read_cell_positions(xls_file)
+        sf = parse_positions(xls_file)
+        df = get_cell_positions(sf)
         assert type(df) == gp.geodataframe.GeoDataFrame
         assert len(df) == 4579
         assert list(df.columns) == ['id', 'point']
-        assert df.loc[0].id == 'prg1_dk0'
-        assert round(df.loc[0].point.x,3) == 21.794 
-        assert round(df.loc[0].point.y,3) == 138.72
+        assert df['id'][0] == 'prg1_dk0'
+        assert round(df['point'][0].x,3) == 21.794 
+        assert round(df['point'][0].y,3) == 138.72
+
+    def test_measurement_positions(self, xls_file):
+        """
+        Test the get_measurements function, which gets x and y coordinates
+        of measurement points and returns them in a GeoPandas frame
+        """
+        sf = parse_positions(xls_file)
+        df = get_measurements(sf)
+        assert type(df) == gp.geodataframe.GeoDataFrame
+        assert len(df) == 17
+        assert list(df.columns) == ['name', 'point']
+        assert df['name'][0] == 'A'
+        assert round(df['point'][0].x,3) == 38.501
+        assert round(df['point'][0].y,3) == 141.542
